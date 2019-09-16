758357ecf8c898e39de41508157d7b3fd2f1ca73
DimaGashko <dmitry.gashko@postindustria.com>
2019-09-16 23:25:03 +0300

    chart: first render

--- 1.chart/src/app/index.ts
+++ 1.chart/src/app/index.ts
@@ -1,32 +1,33 @@
 import { throttle } from 'throttle-debounce';
+import Vector from '../scripts/Vector';
 
 interface IAppElements {
-    canvas: HTMLCanvasElement,
+    canvas?: HTMLCanvasElement,
     [type: string]: HTMLElement
 }
 
-interface IFunc { 
+interface IFunc {
     (x: number): number
 }
 
 export default class App {
     private ctx: CanvasRenderingContext2D = null;
-
-    private $: IAppElements = {
-        canvas: null,
-    };
+    private $: IAppElements = {};
 
     private animationFrameId = 0;
 
-    private width = 0;
-    private height = 0;
+    private size = new Vector(0, 0);
+    private coords = new Vector(0, 0);
+    private zoom = new Vector(50, 25);
+
+    private pointsInterval = 1;
 
     constructor(private root: HTMLElement, private func: IFunc) {
         this.init();
         this.initEvents();
     }
 
-    init() {
+    private init() {
         this.getElements();
         this.resize();
 
@@ -35,56 +36,102 @@ export default class App {
 
     start() {
         if (this.animationFrameId) return;
+        const app = this;
 
         this.animationFrameId = requestAnimationFrame(function tik() {
-            this.clearCanvas();
-            this.tik();
-            requestAnimationFrame(tik.bind(this));
-        }.bind(this));
+            app.clearCanvas();
+            app.tik();
+            requestAnimationFrame(tik);
+        });
     }
 
-    stop() { 
+    stop() {
         cancelAnimationFrame(this.animationFrameId);
         this.animationFrameId = 0;
     }
 
-    tik() { 
-        const w = this.width / 2;
-        const h = this.height / 2;
-        const t = Date.now() / 200;
-        const x = (t % w) * Math.cos(t) + w
-        const y = (t % h) * Math.sin(t) + h;
-
-        this.ctx.fillStyle = '#fff';
-        this.ctx.fillRect(x, y, 16, 16);
+    private tik() {
+        this.renderFunc();
     }
 
-    initEvents() {
+    private renderFunc() { 
+        const ctx = this.ctx;
+        ctx.save();
+
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 1;
+
+        ctx.beginPath();
+
+        this.getPoints().forEach((point, i) => { 
+            const type = (i === 0) ? 'moveTo' : 'lineTo';
+            ctx[type](point.x, point.y);
+        });
+
+        ctx.stroke();
+        ctx.restore();
+    }
+
+    private getPoints() {
+        const interval = this.pointsInterval;
+        const steps = Math.ceil(this.size.x / interval) + 1;
+
+        const xs = new Array(steps).fill(0).map((_, i) => i * interval);
+        const ys = xs.map(x => this.xToWorld(x))
+            .map(x => this.func(x))
+            .map(y => this.yToView(y));
+
+        return xs.map((x, i) => new Vector(x, ys[i]));
+    }
+    /**
+     * Convert viewport coords to world coords
+     * @param coords viport coordinates
+     */
+    private toWorld(coords: Vector) {
+        const offset = this.size.copy().div(new Vector(2, 2));
+        return coords.copy().sub(offset).div(this.zoom).add(this.coords);
+    }
+
+    /**
+     * Convert world coords to viewport coords
+     * @param coords world coordinates
+     */
+    private toView(coords: Vector) {
+        const offset = this.size.copy().div(new Vector(2, 2));
+        return coords.copy().sub(this.coords).mul(this.zoom).add(offset);
+    }
+
+    private xToWorld(x: number) { return this.toWorld(new Vector(x, 0)).x; }
+    private yToWorld(y: number) { return this.toWorld(new Vector(0, y)).y; }
+    private xToView(x: number) { return this.toView(new Vector(x, 0)).x; }
+    private yToView(y: number) { return this.toView(new Vector(0, y)).y; }
+
+    private initEvents() {
         window.addEventListener('resize', throttle(100, () => {
             this.resize();
         }));
     }
 
-    resize() {
+    private resize() {
         this.updateMetrics();
         this.updateCanvasSize();
     }
 
-    updateMetrics() {
-        this.width = this.root.offsetWidth;
-        this.height = this.root.offsetHeight;
+    private updateMetrics() {
+        this.size.x = this.root.offsetWidth;
+        this.size.y = this.root.offsetHeight;
     }
 
-    updateCanvasSize() {
-        this.$.canvas.width = this.width;
-        this.$.canvas.height = this.height;
+    private updateCanvasSize() {
+        this.$.canvas.width = this.size.x;
+        this.$.canvas.height = this.size.y;
     }
 
-    clearCanvas() { 
-        this.ctx.clearRect(0, 0, this.width, this.height);
+    private clearCanvas() {
+        this.ctx.clearRect(0, 0, this.size.x, this.size.y);
     }
 
-    getElements() {
+    private getElements() {
         this.$.canvas = this.root.querySelector('.app__canvas');
         this.ctx = this.$.canvas.getContext('2d');
     }
--- 1.chart/src/index.ts
+++ 1.chart/src/index.ts
@@ -7,5 +7,5 @@ const root: HTMLElement = document.querySelector('.app');
 new App(root, func);
 
 function func(x: number): number {
-    return Math.sin(x);
+    return x % 2;
 }
