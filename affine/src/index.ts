be4769aec5d594c1c3bbd726696d2be2443d9f78
DimaGashko <dmitry.gashko@postindustria.com>
2019-10-04 11:58:52 +0300

    affine: init

---
+++ affine/src/index.ts
@@ -0,0 +1,277 @@
+import 'normalize.scss/normalize.scss';
+import './index.scss';
+
+import { throttle } from 'throttle-debounce';
+import Hammer from 'hammerjs';
+import * as dat from 'dat.gui';
+
+import _font from './font';
+import Vector from './scripts/Vector';
+
+type Line = [number, number, number, number];
+type Char = Line[];
+
+type Vector2 = [number, number];
+
+interface Font {
+    size: number,
+    chars: {
+        symbol: string,
+        lines: Char,
+    }[],
+    unknown: Char,
+}
+
+const $: {
+    canvas?: HTMLCanvasElement,
+    [type: string]: HTMLElement
+} = {};
+
+$.root = document.querySelector('.app');
+$.canvas = $.root.querySelector('.app__canvas');
+
+const ctx = $.canvas.getContext('2d');
+const gui = new dat.GUI();
+
+const pressedKeys: { [keycode: string]: boolean } = {}
+
+const font: Font = _font;
+const charMap = font2CharMap(font);
+
+const KEYS = {
+    left: 37,
+    top: 38,
+    right: 39,
+    bottom: 40,
+}
+
+const options = {
+    color: '#c00',
+    gridColor: '#eee',
+    text: 'A5',
+    letterSpacing: 0.8,
+    worldZoom: 1,
+    resetCoords: () => {
+        coords = initialCoords.copy();
+    }
+}
+
+const screenSize = new Vector(0, 0);
+const initialCoords = new Vector(50, 50);
+let coords = initialCoords.copy();
+
+resize();
+start();
+initGui();
+initEvents();
+
+function start() {
+    requestAnimationFrame(function tik() {
+        clearCanvas();
+        drawFrame();
+
+        requestAnimationFrame(tik);
+    });
+}
+
+function drawFrame() {
+    ctx.save();
+
+    initStyles();
+    useKeyboard();
+
+    drawGrid();
+    draw();
+
+    ctx.restore();
+}
+
+function initEvents() {
+    const hammer = new Hammer($.canvas);
+    hammer.get('pinch').set({ enable: true });
+
+    let startCoords: Vector;
+
+    hammer.on('pan panstart', ({ deltaX, deltaY, type }) => {
+        if (type === 'panstart') {
+            startCoords = coords.copy();
+        }
+
+        const z = new Vector(options.worldZoom, options.worldZoom);
+        const delta = new Vector(-deltaX, -deltaY).div(z);
+
+        coords = startCoords.copy().add(delta);
+    });
+
+    $.canvas.addEventListener('mousewheel', (e: MouseWheelEvent) => {
+        const delta = -e.deltaY / 200;
+        const newZoom = Math.min(Math.max(options.worldZoom + delta, 0.4), 10);
+
+        options.worldZoom = newZoom;
+    });
+
+    $.canvas.addEventListener('keydown', ({ keyCode }) => {
+        pressedKeys[keyCode] = true;
+    });
+
+    $.canvas.addEventListener('keyup', ({ keyCode }) => {
+        pressedKeys[keyCode] = false;
+    });
+
+    window.addEventListener('resize', throttle(100, () => {
+        resize();
+    }));
+
+    window.addEventListener('load', () => {
+        resize();
+    });
+}
+
+function draw() {
+    ctx.translate(coords.x, coords.y);
+    drawText(options.text.toUpperCase());
+}
+
+function drawText(text: string) {
+    text.split('').forEach((realChar) => {
+        const char = (charMap.has(realChar))
+            ? charMap.get(realChar) : font.unknown;
+
+        drawCharacter(char);
+        drawLetterSpace();
+    });
+}
+
+function drawLetterSpace() {
+    const z = options.worldZoom;
+    ctx.translate((font.size * options.letterSpacing * z) ^ 0, 0)
+}
+
+function drawCharacter(char: Char) {
+    char.forEach((l) => {
+        drawLine(l[0], l[1], l[2], l[3]);
+    });
+}
+
+function drawLine(x1: number, y1: number, x2: number, y2: number) {
+    const incX = Math.sign(x2 - x1);
+    const incY = Math.sign(y2 - y1);
+
+    const dx = Math.abs(x2 - x1);
+    const dy = Math.abs(y2 - y1);
+
+    const d = (dx > dy) ? dx : dy;
+
+    let x = x1;
+    let y = y1;
+
+    let xErr = 0;
+    let yErr = 0;
+
+    drawPixel(x, y);
+
+    for (let i = 0; i < d; i++) {
+        xErr += dx;
+        yErr += dy;
+
+        if (xErr > d) {
+            xErr -= d;
+            x += incX;
+        }
+
+        if (yErr > d) {
+            yErr -= d;
+            y += incY;
+        }
+
+        drawPixel(x, y);
+    }
+}
+
+function drawPixel(x, y) {
+    const coords = world2View(new Vector(x, y));
+    const z = options.worldZoom;
+
+    ctx.fillRect(coords.x, coords.y, z, z);
+}
+
+function drawGrid() {
+    ctx.save();
+    ctx.strokeStyle = options.gridColor;
+
+
+
+    ctx.restore();
+}
+
+/**
+ * Convert viewport coords to world coords
+ * @param coords viport coordinates
+ */
+function view2World(targetCoords: Vector) {
+    return targetCoords.copy()
+        .sub(screenSize.copy().div(new Vector(2, 2)))
+        .div(new Vector(options.worldZoom, options.worldZoom))
+        .add(coords);
+}
+
+/**
+ * Convert world coords to viewport coords
+ * @param targetCoords world coordinates
+ */
+function world2View(targetCoords: Vector) {
+    return targetCoords.copy()
+        .sub(coords)
+        .mul(new Vector(options.worldZoom, options.worldZoom))
+        .add(screenSize.copy().div(new Vector(2, 2)));
+}
+
+function useKeyboard() {
+    const step = 8;
+
+    if (pressedKeys[KEYS['left']]) coords.x -= step;
+    if (pressedKeys[KEYS['top']]) coords.y -= step;
+    if (pressedKeys[KEYS['right']]) coords.x += step;
+    if (pressedKeys[KEYS['bottom']]) coords.y += step;
+}
+
+function initStyles() {
+    ctx.fillStyle = options.color;
+}
+
+function resize() {
+    updateMetrics();
+    updateCanvasSize();
+}
+
+function updateMetrics() {
+    screenSize.x = $.root.offsetWidth;
+    screenSize.y = $.root.offsetHeight;
+}
+
+function updateCanvasSize() {
+    $.canvas.width = screenSize.x;
+    $.canvas.height = screenSize.y;
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, screenSize.x, screenSize.y);
+}
+
+function font2CharMap(font: Font) {
+    const map: Map<string, Char> = new Map();
+
+    font.chars.forEach((charConfig) => {
+        map.set(charConfig.symbol, charConfig.lines);
+    });
+
+    return map;
+}
+
+function initGui() {
+    gui.addColor(options, 'color');
+    gui.add(options, 'text');
+    gui.add(options, 'worldZoom', 0.4, 10, 0.1)
+    gui.add(options, 'letterSpacing', 0.5, 2.5, 0.1);
+    gui.add(options, 'resetCoords');
+}
---
+++ affine/src/scripts/Vector.ts
@@ -0,0 +1,39 @@
+
+export default class Vector {
+   constructor(public x: number = 0, public y: number = 0) {
+
+   }
+
+   copy() { 
+      return new Vector(this.x, this.y);
+   }
+
+   add(v: Vector) { 
+      this.x += v.x;
+      this.y += v.y;
+
+      return this;
+   }
+
+   sub(v: Vector) { 
+      this.x -= v.x;
+      this.y -= v.y;
+
+      return this;
+   }
+
+   mul(v: Vector) { 
+      this.x *= v.x;
+      this.y *= v.y;
+
+      return this;
+   }
+
+   div(v: Vector) {
+      this.x /= v.x;
+      this.y /= v.y;
+
+      return this;
+   }
+
+}
