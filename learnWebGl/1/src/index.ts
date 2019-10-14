e339e688f2ae9f30b88223b42307120f43deaeba
DimaGashko <dmitry.gashko@postindustria.com>
2019-10-15 02:29:47 +0300

    gl: imgage processing

--- learnWebGl/1/src/index.ts
+++ learnWebGl/1/src/index.ts
@@ -10,6 +10,8 @@ import fShaderSource from './shaders/f.glsl';
 import Vector from '../src/scripts/Vector';
 import { createShader, createProgram } from './scripts/webGlUtils';
 
+import src from './assets/img.jpeg';
+
 const $: {
     canvas?: HTMLCanvasElement,
     [type: string]: HTMLElement
@@ -34,16 +36,62 @@ const program = createProgram(gl, vShader, fShader);
 
 const locations = {
     aPosition: gl.getAttribLocation(program, 'a_position'),
+    texCoord: gl.getAttribLocation(program, 'a_texCoord'),
     resolution: gl.getUniformLocation(program, 'resolution'),
     time: gl.getUniformLocation(program, 'time'),
 }
-
 const positionBuffer = gl.createBuffer();
+const texCoordBuffer = gl.createBuffer();
+
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-initEvents();
-resize();
-start();
+gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    0.0, 0.0,
+    1.0, 0.0,
+    0.0, 1.0,
+    0.0, 1.0,
+    1.0, 0.0,
+    1.0, 1.0]), gl.STATIC_DRAW);
+
+const img = new Image();
+
+const texture = gl.createTexture();
+gl.bindTexture(gl.TEXTURE_2D, texture);
+
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+img.onload = () => {
+    console.log('hiy');
+    initEvents();
+    resize();
+    start();
+}
+
+img.onerror = () => { 
+    console.log('err');
+}
+
+img.src = src;
+
+function setRectangle(gl, x, y, width, height) {
+    const x1 = x;
+    const x2 = x + width;
+    const y1 = y;
+    const y2 = y + height;
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2,
+    ]), gl.STATIC_DRAW);
+}
 
 function initEvents() {
     window.addEventListener('resize', throttle(50, () => {
@@ -68,21 +116,21 @@ function drawFrame() {
 
     gl.useProgram(program);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.enableVertexAttribArray(locations.aPosition);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.vertexAttribPointer(locations.aPosition, 2, gl.FLOAT, false, 0, 0);
+
+    const ratio = img.width / img.height;
+    setRectangle(gl, 0, 0, screenSize.y * ratio, screenSize.y);
+
+    gl.enableVertexAttribArray(locations.texCoord);
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+    gl.vertexAttribPointer(locations.texCoord, 2, gl.FLOAT, false, 0, 0);
 
     gl.uniform2f(locations.resolution, gl.canvas.width, gl.canvas.height);
     gl.uniform1f(locations.time, performance.now() / 1000);
-    
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        50, 50,
-        500, 100,
-        300, 300,
-        500, 500,
-        900, 600,
-        200, 200,
-    ]), gl.STATIC_DRAW);
+
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
 
     gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
--- learnWebGl/1/src/shaders/f.glsl
+++ learnWebGl/1/src/shaders/f.glsl
@@ -1,10 +1,15 @@
 
 precision mediump float;
 
+uniform sampler2D image;
 uniform float time;
 
 varying vec4 c;
+varying vec2 texCoord;
 
 void main() {
-  gl_FragColor = vec4(0,1,1,1);
+  gl_FragColor = texture2D(image, vec2(
+    texCoord.x, 
+    texCoord.y + sin(time / 10.0)
+  ));
 }
