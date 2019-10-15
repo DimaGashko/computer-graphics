1bc3b2f8e51da2cc504a4e886717032aed136262
DimaGashko <dmitry.gashko@postindustria.com>
2019-10-15 03:03:24 +0300

    gl: videos

--- learnWebGl/1/src/shaders/f.glsl
+++ learnWebGl/1/src/shaders/f.glsl
@@ -9,7 +9,7 @@ varying vec2 texCoord;
 
 void main() {
   gl_FragColor = texture2D(image, vec2(
-    texCoord.x, 
-    texCoord.y + sin(time / 10.0)
+    texCoord.x * 0.9 + cos((time + texCoord.y) / 100.0) / 1000.0, 
+    texCoord.y * 0.8 + sin(time + texCoord.x) * 0.04
   ));
 }
