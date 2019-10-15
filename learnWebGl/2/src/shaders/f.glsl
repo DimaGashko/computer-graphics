b2421e428d490897bcd62f636e7479d3fc6084bc
DimaGashko <dmitry.gashko@postindustria.com>
2019-10-16 01:48:48 +0300

    gl: animate two textures

--- learnWebGl/2/src/shaders/f.glsl
+++ learnWebGl/2/src/shaders/f.glsl
@@ -11,11 +11,14 @@ varying vec2 texCoord;
 //varying vec2 tex2Coord;
 
 void main() {
-   vec4 c = texture2D(image, texCoord);
+   vec4 c = texture2D(image, vec2(
+     texCoord.x + cos(texCoord.y * 20.0 + time * 2.13) / 500.0,
+     texCoord.y + sin(texCoord.y * 20.0 + time * 0.8) / 1000.0
+   ));
 
    vec4 c2 = texture2D(image2, vec2(
-     texCoord.x + sin((texCoord.y * 2.0 + time) * 5.0) / 1000.0,
-     texCoord.y + cos(texCoord.x * 2.0 + time) / 20.0
+     texCoord.x + sin((texCoord.y * 3.0 + time) * 5.0) / 1000.0,
+     texCoord.y + cos(texCoord.x * 2.0 + time) / 25.0
    ));
 
    if (c2.a == 0.0) {
@@ -23,4 +26,6 @@ void main() {
    } else {
      gl_FragColor = vec4(c2.rgb, 1);
    }
+
+   
 }
