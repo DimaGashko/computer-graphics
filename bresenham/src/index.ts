f014bc314a779f93b97f4df70d79235f6af28104
Дмитрий Гашко <dimagashko@gmail.com>
2019-10-31 00:21:04 +0200

    Update index.ts

--- bresenham/src/index.ts
+++ bresenham/src/index.ts
@@ -158,8 +158,7 @@ function drawLine(x1: number, y1: number, x2: number, y2: number) {
 
     const dx = Math.abs(x2 - x1);
     const dy = Math.abs(y2 - y1);
-
-    const d = (dx > dy) ? dx : dy;
+    const d = Math.max(dx, dy);
 
     let x = x1;
     let y = y1;
